009eb3ec9215c833bfe7d25bce3055fb82764016
RozaKerobyan <rozaqeropyan1234@gmail.com>
2024-04-02 22:03:27 +0400

    days-challenge

---
+++ Day 3/tasks.py
@@ -0,0 +1,21 @@
+print("Task 1:")
+name = input("Enter your name: ")
+
+print("Hello "+name+" jan")
+
+print("--------")
+
+print("Task 2:")
+hour = float(input("Enter hour:"))
+rate = float(input("Enter rate:"))
+pay = hour*rate
+print(pay)
+
+print("--------")
+print("Task 3:")
+
+print("This program Celsius convert to Fahrenheit")
+
+celsius = float(input("Enter Celsius"))
+fahreheit = (celsius*1.8) + 32
+print(fahreheit)
