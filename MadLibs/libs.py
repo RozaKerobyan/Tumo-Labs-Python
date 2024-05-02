12e96d8764ac3447b8780bd7f97d2a5b5c96753f
Roza Kerobyan <97788153+RozaKerobyan@users.noreply.github.com>
2024-05-02 18:13:22 +0400

    Update libs.py

--- MadLibs/libs.py
+++ MadLibs/libs.py
@@ -1,12 +1,17 @@
 import random
-from mad import input_template_one, input_template_one
+from mad import input_template_one, input_template_two
 print("Hi its's Mad Libs game")
 print("================")
+
+# Three different templates for the Mad Libs game
 template_1 = ("1: It was about (Number) (Measure of time) ago when I arrived at the hospital in a (Mode of Transportation). The hospital is a/an (Adjective) place, there are a lot of (Adjective2) (Noun) here. There are nurses here who have (Color) (Part of the Body ). If someone wants to come into my room I told them that they have to (Verb) first. I’ve decorated my room with (Number2) (Noun2). Today I talked to a doctor and they were wearing a (Noun3) on their ( Part of the Body 2). I heard that all doctors (Verb) (Noun4) every day for breakfast. The most ( Adjective3) thing about being in the hospital is the (Silly Word ) (Noun) !")
 template_2 = ("2: This weekend I am going camping with ( Proper Noun (Person’s Name)). I packed my lantern, sleeping bag, and (Noun). I am so (Adjective (Feeling)) to (Verb) in a tent. I am (Adjective (Feeling) 2) we might see a(n) (Animal), I hear they’re kind of dangerous. While we’re camping, we are going to hike, fish, and (Verb2). I have heard that the (Color) lake is great for ( Verb (ending in ing) ). Then we will (Adverb (ending in ly)) hike through the forest for (Number) (Measure of Time). If I see a (Color) (Animal) while hiking, I am going to bring it home as a pet! At night we will tell (Number) (Silly Word) stories and roast (Noun2) around the campfire!!")
 template_3 =("3: Dear (Proper Noun (Person’s Name) ), I am writing to you from a (Adjective) castle in an enchanted forest. I found myself here one day after going for a ride on a (Color) (Animal) in (Place). There are (Adjective2) (Magical Creature (Plural)) and (Adjective3) (Magical Creature (Plural)2) here! In the ( Room in a House) there is a pool full of (Noun). I fall asleep each night on a (Noun2) of (Noun(Plural)3) and dream of (Adjective4) ( Noun (Plural)4). It feels as though I have lived here for (Number) ( Measure of time). I hope one day you can visit, although the only way to get here now is (Verb (ending in ing)) on a (Adjective5) (Noun5)!!")
+
+# Randomly select a template
 random_numbers = random.randint(1,3)
 
+# Display the selected template and prompt for user input accordingly
 if (random_numbers == 1):
     print("Your template is ===>>> " + template_1)
     print("================================")
@@ -38,6 +43,8 @@ elif (random_numbers == 3):
     verb_ing = input("Input a verb + ing: ")
     adjective_5 = input("Input adjective: ")
     noun3 = input("Input noun: ")
+
+    # Display the entered story
     print("================================")
     print("The story you entered...")
     print("================================")
